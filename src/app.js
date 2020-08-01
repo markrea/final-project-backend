94d308cbcdce1740812d97023eb58b863b8562b7
Mark Rea <markrea366@gmail.com>
2020-08-01 18:02:38 +0100

    added endpoint for a random recipe

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const fetch = require('node-fetch');
+const dotenv = require('dotenv').config();
 
+const API_KEY3 = process.env.API_KEY3;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({
-    message: 'Hello world!',
-  });
+app.get('/recipes/:ingredients', async (req, res) => {
+  const { ingredients } = req.params;
+  const apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${API_KEY3}&ingredients=${ingredients}&number=2`;
+  const fetchResponse = await fetch(apiUrl);
+  const json = await fetchResponse.json();
+  res.json(json);
+});
+
+app.get('/recipes/:id/moreDetails', async (req, res) => {
+  const { id } = req.params;
+  const apiUrl = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY3}`;
+  const fetchResponse = await fetch(apiUrl);
+  const json = await fetchResponse.json();
+  res.json(json);
+});
+
+app.get('/recipe/random', async (req, res) => {
+  const apiUrl = `https://api.spoonacular.com/recipes/random?number=1&apiKey=${API_KEY3}`;
+  const fetchResponse = await fetch(apiUrl);
+  const json = await fetchResponse.json();
+  res.json(json);
 });
 
 module.exports = app;
