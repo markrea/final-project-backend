d1b3295ed57e9fcd2139493f5cdd98118a0694c6
Mark Rea <markrea366@gmail.com>
2020-08-05 22:43:43 +0100

    changed recipeByName endpoint

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -4,6 +4,6 @@ const recipeController = require('../controllers/recipes');
 const router = express.Router();
 
 router.get('/recipes', recipeController.getRecipeByIngredients);
-router.get('/:recipename', recipeController.getRecipeByName);
+router.get('/recipename', recipeController.getRecipeByName);
 
 module.exports = router;
