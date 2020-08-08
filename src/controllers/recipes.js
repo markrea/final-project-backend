2bbcb32a6adbebce29ef3ab43129ba27f956ddc7
Mark Rea <markrea366@gmail.com>
2020-08-08 16:36:58 +0100

    added intolerances to search by recipe

--- src/controllers/recipes.js
+++ src/controllers/recipes.js
@@ -15,7 +15,7 @@ exports.getRecipeByName = async (req, res) => {
   const { query } = req.query;
   const { diet } = req.query;
   const intolerances = req.query.intoleranceValue;
-  const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&diet=${diet}&intolerances=${intolerances}addRecipeNutrition=true&apiKey=${API_KEY3}&number=2`;
+  const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&diet=${diet}&intolerances=${intolerances}&addRecipeNutrition=true&apiKey=${API_KEY3}&number=2`;
   const fetchResponse = await fetch(apiUrl);
   const json = await fetchResponse.json();
   res.status(200).json(json);
